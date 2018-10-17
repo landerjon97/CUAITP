a439f28af0ad356152c65f9bb80cecf967fedf3a
jlander <lander.jon9@gmail.com>
2018-10-17 12:08:25 -0400

    intial commit

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,url_for
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 with app.test_request_context():
     url = url_for('static',filename='main.css')
