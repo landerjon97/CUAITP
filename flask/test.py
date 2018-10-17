d5fea06802ffb8704d130c0be690ae0e2ca8697b
jlander <lander.jon9@gmail.com>
2018-10-17 10:00:18 -0400

    copy of current aitp

---
+++ flask/test.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template,url_for
+
+app = Flask(__name__, static_url_path='/static')
+
+with app.test_request_context():
+    url = url_for('static',filename='main.css')
+@app.route("/")
+def index():
+    return render_template('index.html')
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
+@app.route("/minutes")
+def minutes():
+  return render_template('minutes.html')
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
